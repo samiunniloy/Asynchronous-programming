0e1fa2f8aa0e4567fcda154c25d1d21cf306c879
Samiun Islam <60150274+samiunniloy@users.noreply.github.com>
2025-03-25 13:01:02 +0600

    sync

--- obj/Release/net8.0/Asynchronous programming.AssemblyInfo.cs
+++ obj/Release/net8.0/Asynchronous programming.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asynchronous programming")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dfb06a91003a58ff2c3379e14ea70c6bd2db810")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asynchronous programming")]
